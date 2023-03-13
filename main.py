cde452445fa10708c17566d87dee2a050f53c0c0
slothory <126821929+slothory@users.noreply.github.com>
2023-03-13 01:59:40 +0000

    Added variables that include the questions and their answers

---
+++ main.py
@@ -0,0 +1,3 @@
+questions = ["What is the main subject studied in B-block?", "What is the main subject studied in E-block?", "What is the main subject studied in H-block?", "What is the main subject studied in G-block?", "What is the main subject studied in C-block?", "What is the main subject studied in F-block?", "What is the main subject studied in A-block?", "What is the main subject studied in K-block?", "What is the main subject studied in the RHS gyms?", "What is the main subject studied in P-block?", ]
+answers = ["Tech", "Tech", "Food", "English", "Math", "Science", "Art", "Language", "P.E", "Science"]
+
