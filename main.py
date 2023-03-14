5ae0e83953b58e64b5461412d431c8711873c6aa
slothory <126821929+slothory@users.noreply.github.com>
2023-03-14 22:22:34 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-#what the calculator will output
-def my_math_function(x, y, z):
-  if z=="M":
-    return x*y
-  if z=="D":
-    return x/y
-  if z=="A":
-    return x+y
-  if z=="S":
-    return x-y
+#this is where the questions and their answers are stored
+questions = ["What is the main subject studied in B-block?", "What is the main subject studied in E-block?", "What is the main subject studied in H-block?", "What is the main subject studied in G-block?", "What is the main subject studied in C-block?", "What is the main subject studied in F-block?", "What is the main subject studied in A-block?", "What is the main subject studied in K-block?", "What is the main subject studied in the RHS gyms?", "What is the main subject studied in P-block?", ]
+answers = ["Tech", "Tech", "Food", "English", "Math", "Science", "Art", "Language", "P.E", "Science"]
 
+#Main code
+score = 0   
 
-#the main code for the calculator
-print("-----------------------------------")
-print("= Welcome to my python calculator =")
-print("-----------------------------------")
-math_sign=input("What equation would you like mo to complete. D = Division, M = Multiplication, A = Addition or S = Subtraction? ")
-num1=input("Enter first number ")
-num2=input("Enter second number ")
-print("Your final answer is") 
-print(my_math_function(int(num1),
-int(num2), math_sign))
+for i in range(len(questions)):
+    print("Question", i+1, ":", questions[i])
+    user_answer = input("Your answer: ")
+    if user_answer.lower() == answers[i].lower():
+        print("Correct!")
+        score += 1   #adds one to the score
+    else:
+        print("Incorrect. The correct answer is", answers[i])    #doesn't add one to the score because you got it wrong
+
+print("Your final score is:", score, "out of", len(questions))    #calculates the amount of questions you got right and how many you got wrong.
+
